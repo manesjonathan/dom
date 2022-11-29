17394bc3f0a22926696912cd816c20666162ff34
Jonathan Manes <manesjona@gmail.com>
2022-11-29 10:11:26 +0100

    Exercise 1

---
+++ Exercise 1/script.js
@@ -0,0 +1,28 @@
+// Get title h1 and display it in console
+let body = document.body;
+let title = body.getElementsByTagName("h1")[0];
+console.log(title.textContent);
+
+// Change title and display it in console
+title.textContent = "Modifying the DOM";
+console.log(title.textContent);
+
+
+// Change bg color with hot pink
+body.style.backgroundColor = "#FF69B4";
+
+// Change bg color with a random rgb color
+body.style.backgroundColor = getRandomColor();
+
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+
+    return ("rgb(" + r + "," + g + "," + b + ")")
+}
+
+// Display in console each nodes of body (second document child)
+for (node of body.childNodes){
+    console.log(node)
+}
